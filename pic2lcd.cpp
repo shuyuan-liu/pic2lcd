b274c83ebb2d153ebcace78f1441b1c4d9574c63
Liu Shuyuan <liu_shuyuan@qq.com>
2020-05-05 23:10:06 +0800

    Change wording of help page and error messages

--- pic2lcd.cpp
+++ pic2lcd.cpp
@@ -15,18 +15,18 @@ using namespace TCLAP;
 int main(int argc, char** argv)
 {
     // Set up command line options
-    CmdLine cmd("pic2lcd: convert images into hex strings to display on monochrome LCD screens like those driven by SSD1306, SH1106, ST7525, ST7920, etc.", ' ', "Beta " __TIME__ " " __DATE__);
+    CmdLine cmd("pic2lcd: Convert images into raw data for monochrome LCD screens like those driven by SSD1306, SH1106, ST7525, ST7920, etc.", ' ', "Beta " __TIME__ " " __DATE__);
 
-    UnlabeledValueArg<string> param_image_file("image", "The image to be processed", true, "", "path_to_image");
+    UnlabeledValueArg<string> param_image_file("image", "The image to convert", true, "", "path_to_image");
     cmd.add(param_image_file);
 
-    ValueArg<string> param_dither_algorithm("", "dither-alg", "The dithering algorithm to use. Available values are jjn, floyd_steinberg, stucki, atikinson, burkes, sierra, sierra_2_row, sierra_lite. The default value is floyd_steinberg.", false, "floyd_steinberg", "name_of_dither_alg");
+    ValueArg<string> param_dither_algorithm("a", "dither-alg", "Select the dithering algorithm to dither the image with. Default: floyd_steinberg. Available: jjn, floyd_steinberg, stucki, atikinson, burkes, sierra, sierra_2_row, sierra_lite.", false, "floyd_steinberg", "dither_alg");
     cmd.add(param_dither_algorithm);
 
-    ValueArg<string> param_delimiter("", "delimiter", "A string separating values in the output. The default is \", \", which outputs \"0x55, 0xAA, ...\".", false, ", ", "delimiter");
+    ValueArg<string> param_delimiter("d", "delimiter", "What goes between values in the output. Default: ', ', which outputs '0x55, 0xAA, ...'.", false, ", ", "delimiter");
     cmd.add(param_delimiter);
 
-    SwitchArg param_decimal("", "decimal", "Use decimal values instead of hex.", false);
+    SwitchArg param_decimal("c", "decimal", "Output decimal values instead of hex.", false);
     cmd.add(param_decimal);
 
     cmd.parse(argc, argv);
@@ -34,7 +34,7 @@ int main(int argc, char** argv)
     // Read the image
     BMP image;
     if(!image.ReadFromFile(param_image_file.getValue().c_str())) {
-        fprintf(stderr, "The image file could not be read.\n");
+        fprintf(stderr, "Error opening the image file.\n");
         return 1;
     }
     size_t width = image.TellWidth();
@@ -43,6 +43,7 @@ int main(int argc, char** argv)
     // Select the specified diterhing algorithm
     if(dither_matrices.find(param_dither_algorithm.getValue().c_str()) == dither_matrices.end()) {
         fprintf(stderr, "The dithering algorithm specified is not one of those available.\n");
+        fprintf(stderr, "Available algorithms: jjn, floyd_steinberg, stucki, atikinson, burkes, sierra, sierra_2_row, sierra_lite.\n");
         return 1;
     }
     auto dither_matrix = dither_matrices[param_dither_algorithm.getValue().c_str()];
@@ -54,6 +55,7 @@ int main(int argc, char** argv)
     //                  pixel in the original image will have one luminance
     //                  value here. Therefore an input image of size 128x64
     //                  will give an image_greyscale with 128*64 elements.
+    // 
     uint_fast8_t *image_greyscale = new uint_fast8_t[width * height];
     for (size_t y = 0; y < height; y++) {
         for (size_t x = 0; x < width; x++) {
@@ -69,8 +71,8 @@ int main(int argc, char** argv)
     //         added to it by its beighbouring pixels. When determining
     //         whether a pixel will be white / black in the final image, 
     //         the value of (the pixel's luminance + added error) is used
-    //         instead of just the luminance itself,  in order to compensate
-    //         for 
+    //         instead of the luminance itself.
+    // 
     int *errors = new int[width * height];
     for (size_t y = 0; y < height; y++) {
         for (size_t x = 0; x < width; x++) {
@@ -95,6 +97,7 @@ int main(int argc, char** argv)
             // dither_matrix: the collection of "spread"s that defines how the
             //                error on the current pixel is divided and added
             //                to its neighbouring pixels.
+            // 
             for (const auto& spread : dither_matrix) {
                 int x_offset,  y_offset; 
                 double proportion;
@@ -106,10 +109,11 @@ int main(int argc, char** argv)
         }
     }
 
-    // Convert the dithered image to a list of values,  each representing
+    // Convert the dithered image to a list of values, each representing
     // 8 pixels (1 byte of data).
     // 
     // Byte arrangement: vertical bytes, LSB up
+    // 
     string delimiter = param_delimiter.getValue();
     bool decimal = param_decimal.getValue();
     bool first_value = true;
