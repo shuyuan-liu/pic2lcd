eec9a99bf06c2c6f88f7e095a668b8846797e03f
Shuyuan Liu <41851763+shuyuan-liu@users.noreply.github.com>
2021-02-14 23:59:57 +0000

    Dithering fully working

--- dither.cpp
+++ dither.cpp
@@ -1,9 +1,10 @@
 #include "dither.hpp"
 #include <cstddef>
 #include <cstring>
-#include <iostream>
 
 
+// I made these variables global because I don't want to be passing them to
+// index_of every time I use it
 int w = 0;
 int h = 0;
 int trash_index = 0;
@@ -11,62 +12,38 @@ int trash_index = 0;
 
 int index_of (int x, int y, int x_offset, int y_offset)
 {
-    if (x + x_offset < 0 || x + x_offset >= w || y + y_offset < 0 || y + y_offset >= h) {
+    if ((x + x_offset) < 0 || (x + x_offset) >= w || (y + y_offset) < 0 || (y + y_offset) >= h) {
         return trash_index;
+    } else {
+        return (y + y_offset) * w + x + x_offset;
     }
-
-    return y_offset * w + x + x_offset;
 }
 
 
 void dither (png::image<png::gray_pixel> &image)
 {
-    // This function implements the JJN dithering algorithm.
-    // The algorithm is taken from:
-    // https://tannerhelland.com/2012/12/28/dithering-eleven-algorithms-source-code.html
-
     w = image.get_width();
     h = image.get_height();
 
-    double *errors = new double[3 * w + 1](); // One extra space for out-of-bounds writes to go to
+    double *errors = new double[w * h + 1](); // One extra space for out-of-bounds writes to go to
     trash_index = sizeof(errors) - 1;
+    
     for (int y = 0; y < h; y++) {
         for (int x = 0; x < w; x++) {
 
             int original_val = image[y][x];
-            int final_val = ((original_val - errors[x]) > 127) ? 255 : 0;
-            double error = final_val - original_val;
+            int corrected_val = original_val - errors[index_of(x, y, 0, 0)];
+            int final_val = (corrected_val > 127) ? 255 : 0;
+            int error = final_val - corrected_val;
             image[y][x] = final_val;
 
-            double error_7 = error * (7.0 / 48.0);
-            double error_5 = error * (5.0 / 48.0);
-            double error_3 = error * (3.0 / 48.0);
-            double error_1 = error * (1.0 / 48.0);
-            std::cerr << "(" << x << ", " << y << "): " << error << ", " << error_7 << ", " << error_5 <<", " << error_3 <<", " << error_1 << std::endl;
-            
-            errors[index_of(x, y,  1, 0)] += error_7;
-            errors[index_of(x, y,  2, 0)] += error_5;
-            errors[index_of(x, y, -2, 1)] += error_3;
-            errors[index_of(x, y, -1, 1)] += error_5;
-            errors[index_of(x, y,  0, 1)] += error_7;
-            errors[index_of(x, y,  1, 1)] += error_5;
-            errors[index_of(x, y,  2, 1)] += error_3;
-            errors[index_of(x, y, -2, 2)] += error_1;
-            errors[index_of(x, y, -1, 2)] += error_3;
-            errors[index_of(x, y,  0, 2)] += error_5;
-            errors[index_of(x, y,  1, 2)] += error_3;
-            errors[index_of(x, y,  2, 2)] += error_1;
+            for (auto k : dither_matrix) {
+                int x_offset = k.first.first;
+                int y_offset = k.first.second;
+                double proportion = k.second;
+                errors[index_of(x, y, x_offset, y_offset)] += error * proportion;
+            }
         }
-
-        // We are progressing to the next line so shift the next line's errors
-        // into the current line's space.
-        std::memmove (errors, errors + w, w * sizeof(double));
-        std::memmove (errors + w, errors + 2 * w, w * sizeof(double));
-        
-
-        // Clear the space for the next line so the previously next-line error
-        // values won't interfere with new ones.
-        std::memset (errors + 2 * w, 0, w * sizeof(double));
     }
 
     delete [] errors;
--- dither.hpp
+++ dither.hpp
@@ -2,6 +2,23 @@
 #define _DITHER_HPP_
 
 #include <png++/png.hpp>
+#include <map>
+#include <tuple>
+
+
+// 
+const std::map<std::pair<int, int>, double> dither_matrix = {
+    {{ 1,  0}, (double) 5 / 32},
+    {{ 2,  0}, (double) 3 / 32},
+    {{-2,  1}, (double) 2 / 32},
+    {{-1,  1}, (double) 4 / 32},
+    {{ 0,  1}, (double) 5 / 32},
+    {{ 1,  1}, (double) 4 / 32},
+    {{ 2,  1}, (double) 2 / 32},
+    {{-1,  2}, (double) 2 / 32},
+    {{ 0,  2}, (double) 3 / 32},
+    {{ 1,  2}, (double) 2 / 32},
+};
 
 
 void dither (png::image<png::gray_pixel> &image);
